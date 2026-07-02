588761baacb3f5172a52e0434efff2280e200070
Spoyna0505321 <111008552+Spoyna0505321@users.noreply.github.com>
2026-07-02 14:59:37 +0300

    reset password and user info fetch

--- users/urls.py
+++ users/urls.py
@@ -1,11 +1,14 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import logout, register, verify, MyTokenObtainPairView
+from .views import logout, register, verify,resetPassword,reset, Profile,MyTokenObtainPairView
 
 urlpatterns = [
+    path("reset_password/", resetPassword),
     path("register/", register),
     path("verify/<uid>/<token>/", verify),
+    path("reset/<uid>/<token>/", reset),
     path("logout/", logout),
     path("login/", MyTokenObtainPairView.as_view()),
     path("refresh/", TokenRefreshView.as_view()),
+    path("profile/",Profile),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view,permission_classes
 from rest_framework.response import Response
 from django.contrib.auth import authenticate
 from django.contrib.auth import get_user_model
@@ -14,6 +14,8 @@ from rest_framework_simplejwt.views import TokenObtainPairView
 from users.serializers import MyTokenObtainPairSerializer, UserSerializer
 from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import status
+from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
 class MyTokenObtainPairView(TokenObtainPairView):
     serializer_class = MyTokenObtainPairSerializer
 User = get_user_model()
@@ -33,7 +35,26 @@ def verify(request ,uid,token):
         return Response({"message": "Email verified successfully"})
     else:
         return Response({"message": "Invalid or expired token"})
+@api_view(["GET","POST"])
+def reset(request ,uid,token):
+    try:
+        
+        uid = urlsafe_base64_decode(uid).decode()
+        user = User.objects.get(pk=uid)
 
+    except:
+        return Response({"message": "Invalid user"})
+    if not default_token_generator.check_token(user, token):
+        return Response({"message": "Invalid or expired token"}, status=400)
+    if request.method == "GET":
+        return render(request, "reset_password.html")
+    password = request.POST.get("password")
+    user.set_password(password)
+    user.save()
+    return Response({"message": "Password changed successfully"})
+
+    
+    
 @api_view(["POST"])
 def logout(request):
     try:
@@ -49,6 +70,28 @@ def logout(request):
 
 
 
+@api_view(["POST"])
+def resetPassword(request):
+    email = request.data.get("email")
+    try:
+        user = User.objects.get(email=email)
+    except User.DoesNotExist:
+        return Response({"message": "User not found"}, status=404)
+   
+    uid = urlsafe_base64_encode(force_bytes(user.pk))
+    token = default_token_generator.make_token(user)
+    link = f"http://172.20.10.8:8080/api/reset/{uid}/{token}/"
+    send_mail(
+            "Reset your password",
+            link,
+            settings.EMAIL_HOST_USER,
+            [email],
+        )
+        
+    return Response({
+            "message": "Password updated",
+            "name": email
+        })
     
 @api_view(["POST"])
 def register(request):
@@ -73,3 +116,16 @@ def register(request):
         })
     else:
         return Response(user.errors, status=400)
+@api_view(["POST"])
+@permission_classes([IsAuthenticated])
+def Profile(request):
+    user = request.user
+    return Response({
+        "id": user.id,
+        "name": user.name,
+        "email": user.email,
+    })
+    
+        
+        
+     
